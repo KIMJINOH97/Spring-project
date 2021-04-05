ebd5faff79257545bbb445e4f89772eebfe8ae23
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-05 14:49:46 +0900

    feat: controller 작동 테스트

---
+++ src/main/java/com/book/spring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.book.spring.controller;
+
+import com.book.spring.controller.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/dto")
+    public HelloResponseDto hello(@RequestParam(name = "name") String name, @RequestParam(name = "amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
+    @GetMapping("/hellodto")
+    public HelloResponseDto hello(@RequestParam HelloResponseDto dto){
+        return dto;
+    }
+}
